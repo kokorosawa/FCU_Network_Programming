7ef52b1034adeda42d67f74258408faa06a2b4f5
kokoro <matt920404@gmail.com>
2023-10-17 19:00:07 +0800

    feat:ui

--- lab4/src/Client.py
+++ lab4/src/Client.py
@@ -1,9 +1,10 @@
 import xmlrpc.client
 
+
 class Client:
     def __init__(self) -> None:
         self.proxy = xmlrpc.client.ServerProxy("http://localhost:8888")
-        
+
     def register(self, username, password):
         res = self.proxy.register(username, password)
         if res:
@@ -12,7 +13,7 @@ class Client:
         else:
             print('Register failed')
         return False
-        
+
     def create(self, topic_name, description, founder_name):
         res = self.proxy.create(topic_name, description, founder_name)
         if res:
@@ -26,7 +27,7 @@ class Client:
         res = self.proxy.subject()
         print(res)
         return res
-        
+
     def reply(self, topic_name, username, content):
         res = self.proxy.reply(topic_name, username, content)
         if res:
@@ -50,6 +51,7 @@ class Client:
             print('Delete failed')
         return False
 
+
 if __name__ == "__main__":
     client = Client()
     client.register('user1', '123')
@@ -57,4 +59,3 @@ if __name__ == "__main__":
     client.subject()
     client.reply('topic1', 'user2', 'content2')
     client.discussion('topic1')
-    client.delete('topic1')
