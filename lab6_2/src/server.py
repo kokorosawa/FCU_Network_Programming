91b516f0ee1c2c6749af28eb1466a152d0b3904e
吳念澤 <d1049174@o365.fcu.edu.tw>
2023-11-15 13:54:15 +0800

    feat: lab6_2 temp

--- lab6_2/src/server.py
+++ lab6_2/src/server.py
@@ -3,17 +3,20 @@ import MySAWSocket
 PORT = 8888
 BUF_Size = 1024
 
+
 def main():
-	# Create a SAWSocket Server 
-	server = MySAWSocket.SAWSocket(8888)		# Listen on port 8888
-	server.accept()
-	
-	for i in range(10):
-		msg = server.receive()
-		print('Receive message: ' + msg.decode('utf-8'))
-	
-	server.close()
+    # Create a SAWSocket Server
+    server = MySAWSocket.SAWSocket(8888)  # Listen on port 8888
+    server.accept()
+
+    for i in range(10):
+        msg = server.receive()
+        print("Receive message: " + msg.decode("utf-8"))
+
+    server.close()
+
+
 # end of main
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
